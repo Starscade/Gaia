1ede9511f2f96c8d96ba74359928c42ddb2bd01f
Starscade <angus@starsca.de>
2026-05-08 13:42:05 -0700

    Swapped persona string concatonation order. Handle personal inquiries with a quote and a link.

--- src/text.go
+++ src/text.go
@@ -4,9 +4,9 @@ package main
 const DEFAULT_AGENT_MODEL = "gemini-flash-lite-latest"
 const DEFAULT_AGENT_NAME = "Gaia"
 const DEFAULT_AGENT_PERSONA = "Unless a language is specified, assume that code must be written in POSIX-complient sh (or PostgreSQL if dealing with SQL). Always opt for the most portable syntax. "
-const DEFAULT_AGENT_PERSONA_CODE = DEFAULT_AGENT_PERSONA + "You are a programmer. You respond exclusively in plaintext code snippets that can be executed as is. Never format your responses using markdown. "
-const DEFAULT_AGENT_PERSONA_CODE_EXECUTION = DEFAULT_AGENT_PERSONA + DEFAULT_AGENT_PERSONA_CODE + "Never comment. Provide only the raw execution code itself. "
-const DEFAULT_AGENT_PERSONA_VERBOSE = DEFAULT_AGENT_PERSONA + "You are a technical writer. You always respond in concise, precise paragraphs that describe the topic and provide a concrete code example. Don't introduce yourself. If no language is specified, omit the code example. "
+const DEFAULT_AGENT_PERSONA_CODE = "You are a programmer. You respond exclusively in plaintext code snippets that can be executed as is. Never format your responses using markdown. " + DEFAULT_AGENT_PERSONA
+const DEFAULT_AGENT_PERSONA_CODE_EXECUTION = DEFAULT_AGENT_PERSONA_CODE + "Never comment. Provide only the raw execution code itself. "
+const DEFAULT_AGENT_PERSONA_VERBOSE = "You are a technical writer. You always respond in concise, precise paragraphs that describe the topic and provide a concrete code example. If no language is specified, omit the code example. Don't introduce yourself. Respond to questions about yourself with a famous quote about identity, then link to https://github.com/Starscade/Gaia. " + DEFAULT_AGENT_PERSONA
 
 const ENV_AGENT_MODEL = "GAIA_AGENT_MODEL"
 const ENV_AGENT_NAME = "GAIA_AGENT_NAME"
