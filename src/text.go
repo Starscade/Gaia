09e5ae3bfc965803db7d6ac2895c22425d993cfc
Starscade <angus@starsca.de>
2026-05-08 11:22:43 -0700

    Changed variables to constants to improve performance.

--- src/text.go
+++ src/text.go
@@ -1,20 +1,20 @@
 package main
 
 
-var DEFAULT_AGENT_MODEL = "gemini-flash-lite-latest"
-var DEFAULT_AGENT_NAME = "Gaia"
-var DEFAULT_AGENT_PERSONA = "You are a programmer. You respond exclusively in plaintext code snippets that can be executed as is. Never format your responses using markdown. Always assume the code must be written in POSIX-complient sh, no bash-isms or zsh or Python, etc. Always opt for the most portable syntax. If writing SQL, use Postgres syntax unless otherwise specified."
-var DEFAULT_AGENT_PERSONA_VERBOSE = "You are a contestant on a general knowledge gameshow. You always answer in concise, precise sentences that fully answer the question. Never acknowledge that you are on a gameshow."
+const DEFAULT_AGENT_MODEL = "gemini-flash-lite-latest"
+const DEFAULT_AGENT_NAME = "Gaia"
+const DEFAULT_AGENT_PERSONA = "You are a programmer. You respond exclusively in plaintext code snippets that can be executed as is. Never format your responses using markdown. Always assume the code must be written in POSIX-complient sh, no bash-isms or zsh or Python, etc. Always opt for the most portable syntax. If writing SQL, use Postgres syntax unless otherwise specified."
+const DEFAULT_AGENT_PERSONA_VERBOSE = "You are a technical writer. You always respond in concise, precise paragraphs that could be drop-in replacements for code documentation."
 
-var ENV_AGENT_MODEL = "GAIA_AGENT_MODEL"
-var ENV_AGENT_PERSONA = "GAIA_AGENT_PERSONA"
-var ENV_AGENT_PERSONA_VERBOSE = "GAIA_AGENT_PERSONA_VERBOSE"
-var ENV_APIKEY = "GEMINI_API_KEY"
+const ENV_AGENT_MODEL = "GAIA_AGENT_MODEL"
+const ENV_AGENT_PERSONA = "GAIA_AGENT_PERSONA"
+const ENV_AGENT_PERSONA_VERBOSE = "GAIA_AGENT_PERSONA_VERBOSE"
+const ENV_APIKEY = "GEMINI_API_KEY"
 
-var ERR_NO_APIKEY = "No GEMINI_API_KEY!"
-var ERR_NO_PROMPT = "No prompt provided!"
+const ERR_NO_APIKEY = "No GEMINI_API_KEY!"
+const ERR_NO_PROMPT = "No prompt provided!"
 
-var FLAG_EXECUTE_FLAG = "x"
-var FLAG_EXECUTE_HELP = "Execute the prompt as code."
-var FLAG_VERBOSE_FLAG = "v"
-var FLAG_VERBOSE_HELP = "Speak casually."
+const FLAG_EXECUTE_OPTION_SHORT = "x"
+const FLAG_EXECUTE_HELP = "Execute the prompt as code."
+const FLAG_VERBOSE_OPTION_SHORT = "v"
+const FLAG_VERBOSE_HELP = "Speak casually."
