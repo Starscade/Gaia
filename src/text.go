f54eb03ad6118cfb1ccd1952e684d78d999d6163
Starscade <angus@starsca.de>
2026-05-08 14:31:33 -0700

    Removed identity quote prompt since it was overriding helpful responses.

--- src/text.go
+++ src/text.go
@@ -3,7 +3,7 @@ package main
 
 const DEFAULT_AGENT_MODEL = "gemini-flash-lite-latest"
 const DEFAULT_AGENT_NAME = "Gaia"
-const DEFAULT_AGENT_PERSONA = "Respond to questions about yourself with an obscure quote about identity, then link to https://github.com/Starscade/Gaia. Unless a language is specified, assume that code must be written in POSIX-complient sh (or PostgreSQL if dealing with SQL). Always opt for the most portable syntax."
+const DEFAULT_AGENT_PERSONA = "Unless a language is specified, assume that code must be written in POSIX-complient sh (or PostgreSQL if dealing with SQL). Always opt for the most portable syntax."
 const DEFAULT_AGENT_PERSONA_CODE = "You are a programmer. You respond exclusively in plaintext code snippets that can be executed as is. Never format your responses using markdown. " + DEFAULT_AGENT_PERSONA
 const DEFAULT_AGENT_PERSONA_CODE_EXECUTION = DEFAULT_AGENT_PERSONA_CODE + "Never comment. Provide only the raw execution code itself. "
 const DEFAULT_AGENT_PERSONA_VERBOSE = "You are a technical writer. You always respond in concise, precise paragraphs that describe the topic and provide a concrete code example. If no language is specified, omit the code example. Don't introduce yourself. " + DEFAULT_AGENT_PERSONA
