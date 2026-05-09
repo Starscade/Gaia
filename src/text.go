24380b8d8228e1bd5a5d676c8b3c2e6eb70f3535
Starscade <angus@starsca.de>
2026-05-09 08:17:56 -0700

    Define all constants with a single block.

--- src/text.go
+++ src/text.go
@@ -1,22 +1,26 @@
 package main
 
 
-const DEFAULT_AGENT_MODEL = "gemini-flash-lite-latest"
-const DEFAULT_AGENT_NAME = "Gaia"
-const DEFAULT_AGENT_PERSONA = "Unless a language is specified, assume that code must be written in POSIX-complient sh (or PostgreSQL if dealing with SQL). Always opt for the most portable syntax."
-const DEFAULT_AGENT_PERSONA_CODE = "You are a programmer. You respond exclusively in plaintext code snippets that can be executed as is. Never format your responses using markdown. " + DEFAULT_AGENT_PERSONA
-const DEFAULT_AGENT_PERSONA_CODE_EXECUTION = DEFAULT_AGENT_PERSONA_CODE + "Never comment. Provide only the raw execution code itself. "
-const DEFAULT_AGENT_PERSONA_VERBOSE = "You are a technical writer. You always respond in concise, precise paragraphs that describe the topic and provide a concrete code example. If no language is specified, omit the code example. Don't introduce yourself. " + DEFAULT_AGENT_PERSONA
+const (
+	DEFAULT_AGENT_MODEL = "gemini-flash-lite-latest"
+	DEFAULT_AGENT_NAME = "Gaia"
+	DEFAULT_AGENT_PERSONA = "Unless a language is specified, assume that code must be written in POSIX-complient sh (or PostgreSQL if dealing with SQL). Always opt for the most portable syntax."
+	DEFAULT_AGENT_PERSONA_CODE = "You are a programmer. You respond exclusively in plaintext code snippets that can be executed as is. Never format your responses using markdown. " + DEFAULT_AGENT_PERSONA
+	DEFAULT_AGENT_PERSONA_CODE_EXECUTION = DEFAULT_AGENT_PERSONA_CODE + "Never comment. Provide only the raw execution code itself. "
+	DEFAULT_AGENT_PERSONA_VERBOSE = "You are a technical writer. You always respond in concise, precise paragraphs that describe the topic and provide a concrete code example. If no language is specified, omit the code example. Don't introduce yourself. " + DEFAULT_AGENT_PERSONA
 
-const ENV_AGENT_MODEL = "GAIA_AGENT_MODEL"
-const ENV_AGENT_NAME = "GAIA_AGENT_NAME"
-const ENV_AGENT_PERSONA = "GAIA_AGENT_PERSONA"
-const ENV_APIKEY = "GEMINI_API_KEY"
+	ENV_AGENT_MODEL = "GAIA_AGENT_MODEL"
+	ENV_AGENT_NAME = "GAIA_AGENT_NAME"
+	ENV_AGENT_PERSONA = "GAIA_AGENT_PERSONA"
+	ENV_APIKEY = "GEMINI_API_KEY"
 
-const ERR_NO_APIKEY = "No " + ENV_APIKEY + "!"
-const ERR_NO_PROMPT = "No prompt provided!"
+	ERR_NO_APIKEY = "No " + ENV_APIKEY + "!"
+	ERR_NO_PROMPT = "No prompt provided!"
 
-const FLAG_EXECUTE_HELP = "Execute the prompt as code."
-const FLAG_EXECUTE_OPTION_SHORT = "x"
-const FLAG_VERBOSE_HELP = "Speak casually."
-const FLAG_VERBOSE_OPTION_SHORT = "v"
+	FLAG_EXECUTE_HELP = "Execute the prompt as code."
+	FLAG_EXECUTE_OPTION_SHORT = "x"
+	FLAG_STDIN_OPTION_SHORT = "i"
+	FLAG_STDIN_HELP = "Accept prompt from stdin."
+	FLAG_VERBOSE_HELP = "Speak casually."
+	FLAG_VERBOSE_OPTION_SHORT = "v"
+)
