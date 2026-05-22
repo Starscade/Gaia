792c2fc55b50ece1f936f4056245e9c1f3fddce1
Starscade <angus@starsca.de>
2026-05-22 08:53:13 -0700

    SemVer now reflects current patch, not one-ahead.

--- scripts/auto-version.sh
+++ scripts/auto-version.sh
@@ -3,8 +3,7 @@
 LATEST_MINOR_VERSION="$(git tag | tail -n 1 | cut -d . -f 1,2)"
 ROOT_VERSION="$(git tag | grep "$LATEST_MINOR_VERSION" | head -n 1)"
 CURRENT_PATCH="$(git rev-list --count "$ROOT_VERSION"..HEAD)"
-ONE_AHEAD="$((1 + $CURRENT_PATCH))"
 GIT_BRANCH="$(git branch | head -n 1 | cut -d' ' -f 2)"
-NEW_VERSION="${LATEST_MINOR_VERSION}.${ONE_AHEAD} (${GIT_BRANCH})"
+NEW_VERSION="${LATEST_MINOR_VERSION}.${CURRENT_PATCH} (${GIT_BRANCH})"
 
 sed -i "s/v[[:digit:]]\.[[:digit:]]\.[[:digit:]] \([^\"]*\)/$NEW_VERSION/" internal/text/text.go
