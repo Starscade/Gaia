88017832fb03ccd99459f5218dabef7b1684b4cd
Starscade <angus@starsca.de>
2026-05-22 11:08:10 -0700

    Added comment to clarify purpose of "git status --porcelain".

--- scripts/auto-version.sh
+++ scripts/auto-version.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+
 LATEST_MINOR_VERSION="$(git tag | tail -n 1 | cut -d . -f 1,2)"
 ROOT_VERSION="$(git tag | grep "$LATEST_MINOR_VERSION" | head -n 1)"
 CURRENT_PATCH="$(git rev-list --count "$ROOT_VERSION"..HEAD)"
 GIT_BRANCH="$(git branch | head -n 1 | cut -d' ' -f 2)"
 NEW_VERSION="${LATEST_MINOR_VERSION}.${CURRENT_PATCH} (${GIT_BRANCH})"
+
+
+# If no change, don't auto-increment.
 
 test -z "$(git status --porcelain)" \
 	|| sed -i \
