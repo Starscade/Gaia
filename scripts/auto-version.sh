bf1058e8539d2e1b5d2f1e561c77ac3fe9ffceac
Starscade <angus@starsca.de>
2026-05-24 08:22:11 -0700

    Removed superfluous newlines.

--- scripts/auto-version.sh
+++ scripts/auto-version.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-
 LATEST_MINOR_VERSION="$(git tag | tail -n 1 | cut -d . -f 1,2)"
 ROOT_VERSION="$(git tag | grep "$LATEST_MINOR_VERSION" | head -n 1)"
 CURRENT_PATCH="$(git rev-list --count "$ROOT_VERSION"..HEAD)"
 GIT_BRANCH="$(git branch | head -n 1 | cut -d' ' -f 2)"
 NEW_VERSION="${LATEST_MINOR_VERSION}.${CURRENT_PATCH} (${GIT_BRANCH})"
 CURRENT_VERSION="$(gaia -version)"
-
 
 # If no change, don't auto-increment.
 
