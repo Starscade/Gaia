f4b78d7ce197b246eeea62f3e5e67e405510357a
Starscade <angus@starsca.de>
2026-05-24 06:32:03 -0700

    Compare new version to current version instead of relying on Git.

--- scripts/auto-version.sh
+++ scripts/auto-version.sh
@@ -6,11 +6,12 @@ ROOT_VERSION="$(git tag | grep "$LATEST_MINOR_VERSION" | head -n 1)"
 CURRENT_PATCH="$(git rev-list --count "$ROOT_VERSION"..HEAD)"
 GIT_BRANCH="$(git branch | head -n 1 | cut -d' ' -f 2)"
 NEW_VERSION="${LATEST_MINOR_VERSION}.${CURRENT_PATCH} (${GIT_BRANCH})"
+CURRENT_VERSION="$(gaia -version)"
 
 
 # If no change, don't auto-increment.
 
-if test -z "$(git status --porcelain)"; then
+if test "$CURRENT_VERSION" != "$NEW_VERSION"; then
 	sed -i \
 	"s/v[[:digit:]]\.[[:digit:]]\.[[:digit:]] \([^\"]*\)/$NEW_VERSION/" \
 	internal/text/text.go
