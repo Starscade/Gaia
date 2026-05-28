a8d2f318c9cb962ed40f1000d041a3e2a3e443bf
Starscade <angus@starsca.de>
2026-05-27 22:13:11 -0700

    Prefixed with current dir.

--- scripts/auto-version.sh
+++ scripts/auto-version.sh
@@ -12,5 +12,5 @@ CURRENT_VERSION="$(gaia -version)"
 if test "$CURRENT_VERSION" != "$NEW_VERSION"; then
 	sed -i \
 	"s/v[[:digit:]]\.[[:digit:]]\.[[:digit:]] \([^\"]*\)/$NEW_VERSION/" \
-	internal/text/text.go
+	./internal/text/text.go
 fi
