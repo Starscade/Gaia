d1b8f1c6cdc4a65e5de62225e49c8b317ff46c11
Starscade <angus@starsca.de>
2026-06-25 18:47:25 -0700

    Increment one patch ahead to take into account the fact that git commit will push an additional patch ahead.

--- scripts/auto-version.sh
+++ scripts/auto-version.sh
@@ -3,8 +3,9 @@
 LATEST_MINOR_VERSION="$(git tag | tail -n 1 | cut -d . -f 1,2)"
 ROOT_VERSION="$(git tag | grep "$LATEST_MINOR_VERSION" | head -n 1)"
 CURRENT_PATCH="$(git rev-list --count "$ROOT_VERSION"..HEAD)"
+NEXT_PATCH="$((1 + $CURRENT_PATCH))"
 GIT_BRANCH="$(git branch | head -n 1 | cut -d' ' -f 2)"
-NEW_VERSION="${LATEST_MINOR_VERSION}.${CURRENT_PATCH} (${GIT_BRANCH})"
+NEW_VERSION="${LATEST_MINOR_VERSION}.${NEXT_PATCH} (${GIT_BRANCH})"
 CURRENT_VERSION="$(gaia --version)"
 
 # If no change, don't auto-increment.
