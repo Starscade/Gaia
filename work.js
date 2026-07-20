32249d65f8131d45cebee8a0fb5de48e3d6cab6a
Starscade <angus@starsca.de>
2026-07-20 11:26:24 -0700

    Incorrectly removed event arg from activation listener.

--- work.js
+++ work.js
@@ -2,7 +2,7 @@ self.addEventListener('install', () => {
 	self.skipWaiting()
 })
 
-self.addEventListener('activate', () => {
+self.addEventListener('activate', (event) => {
 	event.waitUntil(
 		caches.keys().then((keys) =>
 			Promise.all(keys.map((key) => caches.delete(key)))
